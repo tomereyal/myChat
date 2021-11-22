d5108a6524354915d204df0e742c3707031714b0
Tomer Eyal <tomereyal@gmail.com>
2021-11-22 11:30:17 +0200

    set up a server for both http req and res and for ws 2 way stream

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
+import http from "http"
+import WebSocket from "ws";
 import express from "express";
+
 import cors from "cors";
 import path from "path";
 import expressJwt from "express-jwt";
@@ -56,7 +59,29 @@ app.get("/", (req, res) => {
   res.send("Hi there!");
 });
 
-app.listen(PORT, () => console.log(`Server is up at ${PORT}`));
+
+const server = http.createServer(app);
+
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws: WebSocket) => {
+
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message: string) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    //send immediatly a feedback to the incoming connection    
+    ws.send('Hi there, I am a WebSocket server');
+});
+
+
+server.listen(PORT, () => console.log(`Server is up at ${PORT}`));
 
 app.use(errorMiddleware);
 
